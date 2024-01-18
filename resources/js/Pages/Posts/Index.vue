53d8495d415c3b8f22b8327f6877ded1dbd825a6
Yordi Cartagena <yordicartagena24@gmail.com>
2024-01-18 10:55:27 -0400

    Add Pest, simple post lists and tests

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <AppLayout>
+        <Container>
+            <ul class="divide-y">
+                <li
+                    class="px-2 py-4" 
+                    v-for="post in posts"
+                >
+                    <span class="font-bold text-lg text-gray-700 dark:text-gray-300">
+                        {{ post.title }}
+                    </span>
+                </li>
+            </ul>
+        </Container>
+        
+    </AppLayout>
+</template>
+
+<script setup>
+
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Container from "@/Components/Container.vue";
+
+defineProps(['posts']);
+
+</script>
